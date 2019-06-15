db2f00390d1a344943e25fd895361967100159ef
Sauerová Markéta <47824190+sauermar@users.noreply.github.com>
2019-06-15 23:44:22 +0200

    adds random sequence of colors to title every run

--- Code/Intro1.pas
+++ Code/Intro1.pas
@@ -38,10 +38,13 @@ procedure LoadFromFile(fileName:string; title: boolean);
  and displays it on Graphics window}
 var myFile : Text;
   	texttmp: string;
-    i: integer;
+    i,j: integer;
+    colors: array [0..4] of ^longword = ( @red,@orange,@green,@Blue,@Purple);
 begin
-i:= 50;
-setcolor(red);
+Randomize; //generate a new sequence of colors every time the program is run
+j:= Random(4);  //Get a random number between 0 and 4
+i:= 50; // y coordinate of displaying text
+setcolor(colors[j]^);
 Assign(myFile, fileName);
 Reset(myFile);
 SetTextStyle(CourierNewFont,0,1);
@@ -49,27 +52,32 @@ Repeat
   ReadLn(myFile, texttmp);
   if title=true then
   	begin
+      //draws loaded text on window if the title is true
 			OutTextXY(125,i,texttmp);
   		i:= i + TextHeight(texttmp);
+      //changing colors of every line
       if i = 50 + TextHeight(texttmp) then
-      	setcolor(Orange)
+      	setcolor(colors[(j+1) mod 5]^ )
       else if i = 50 + (TextHeight(texttmp)*2) then
-        setcolor(Green)
+        setcolor(colors[(j+2) mod 5]^)
       else if i = 50 + (TextHeight(texttmp)*3) then
-        setcolor(Blue)
-      else setcolor(Purple);
+        setcolor(colors[(j+3) mod 5]^)
+      else setcolor(colors[(j+4) mod 5]^);
   	end;
 until Eof(myfile);
 close(myfile);
 end;
 procedure AnimateTitle();
+{creates a rotation animation of the title}
 const  width = 500;
        height = 50;
-       Pi18 = Pi/18;
+       Pi18 = Pi/18; //slowing the rotation
 var anim  : AnimatType;
     i: integer;
 begin
+   //gets apropriate number of pixels from screen and saves them as anim
   GetAnim(100,30,125+width-1,50+height-1,Transparent,anim);
+  //puts anim bitmap on the screen
   PutAnim(110,40,anim,CopyPut);
   Delay(500);
   i:=0;
